024b3d1b1e4bb5ccdcdb7a75e90c798cefc15e7e
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-07-10 20:02:35 -0500

    Add files via upload

---
+++ PRUEBA APLICACIONES WEB/Tabla 4/TABLA 4.js
@@ -0,0 +1,21 @@
+//Tabla de MUltiplicar 4
+var num = parseInt(prompt("Ingrese el numero de la tabla que desea"))
+for (let i=1; i<13; i++){
+    document.write(num + "x"+ i + "="+(num*i)+"<br>")
+}
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
