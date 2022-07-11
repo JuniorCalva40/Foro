6b5a9b3b5e25ed228913b72a25bfbf1dbefeb7aa
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-07-10 23:46:36 -0500

    Add files via upload

---
+++ Foro/javascript.js
@@ -0,0 +1,23 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+let pares = [];
+let impares = [];
+
+for(let i= 0;i<array.length;i++){
+    if(i % 2 ==0){
+        pares.push(array[i]);
+    }
+    else{
+        impares.push(array[i]);
+    }
+console.log("TRUE  y los pares son" + pares);
+console.log("FALSE y los impares son " + impares);
+
+
+
+ 
+
+
+
+
+
+
